98207c2f1cbdb3544dfe6ba34829aa5b7d6a2d9c
Slava <kuncovs19@gmail.com>
2023-09-26 01:24:50 +0300

    fix 3

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,23 @@
 import react from '@vitejs/plugin-react'
+import reactRefresh from '@vitejs/plugin-react-refresh';
 import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react()],
+	plugins: [react(), reactRefresh()],
+	resolve: {
+		alias: {
+			react: 'react' // Проверьте, что это указывает на правильный путь к модулю React
+		}
+	},
 	build: {
 		rollupOptions: {
-			external: [
-				'react',
-				'react-dom',
-				'redux',
-				'redux-persist/integration/react'
-			]
+			// external: [
+			// 	'react',
+			// 	'react-dom',
+			// 	'redux',
+			// 	'redux-persist/integration/react'
+			// ]
 		}
 	}
 })
