292f627a79f7ca7e0086402e4646921ce9abbf7b
Slava <kuncovs19@gmail.com>
2023-10-15 03:11:40 +0300

    add assets import module

---
+++ src/utils/assetsImports.js
@@ -0,0 +1,7 @@
+export { default as cake } from 'assets/cakes/pngegg.png'
+
+export { default as m1 } from 'assets/cakes/honey cake/1579904333_7-p-torti-medovik-13.jpg'
+export { default as m2 } from 'assets/cakes/honey cake/1680502509_3-95.jpg'
+export { default as m3 } from 'assets/cakes/honey cake/288e6160f1f8aa7d06a3a1abe095ff23.jpeg'
+
+export { default as cakeMobileBlur } from 'assets/blur.png'
