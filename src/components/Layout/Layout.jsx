2aa24e8ffad11770b8dae01b855e8acbef9c183d
Slava <kuncovs19@gmail.com>
2023-10-15 05:04:37 +0300

    add redirect conditions

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,7 +1,7 @@
 import { Suspense, lazy, useEffect } from 'react'
 
 import PropTypes from 'prop-types'
-import { Outlet, useNavigate } from 'react-router-dom'
+import { Outlet, useLocation, useNavigate } from 'react-router-dom'
 
 import 'src/App.scss'
 
@@ -13,10 +13,19 @@ const Cart = lazy(() => import('components/Cart/Cart'))
 export default function Layout() {
 
 	const navigate = useNavigate();
+	const location = useLocation();
 
 	useEffect(() => {
-		navigate('/home', { replace: true });
-	}, [navigate ])
+		if(location.pathname === '/') {
+			navigate('/home', { replace: true });
+		}
+		else if(location.hash === '/catalog') {
+			navigate('/catalog', { replace: true });
+		}
+		else if(location.hash === '/about') {
+			navigate('/about', { replace: true });
+		}
+	}, [navigate, location]);
 
 	return (
 		<div className='layout bg-white w-full h-full overflow-x-hidden overflow-y-auto'>
