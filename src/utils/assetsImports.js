3a612fe0df9a95aeadc1ec848a8b7e485c0fc44e
Slava <kuncovs19@gmail.com>
2023-10-18 02:17:42 +0300

    add and change img

--- src/utils/assetsImports.js
+++ src/utils/assetsImports.js
@@ -4,4 +4,4 @@ export { default as m1 } from 'assets/cakes/honey cake/1579904333_7-p-torti-medo
 export { default as m2 } from 'assets/cakes/honey cake/1680502509_3-95.jpg'
 export { default as m3 } from 'assets/cakes/honey cake/288e6160f1f8aa7d06a3a1abe095ff23.jpeg'
 
-export { default as cakeMobileBlur } from 'assets/blur.png'
+export { default as cakeMobileBlur } from 'assets/blur2.png'
