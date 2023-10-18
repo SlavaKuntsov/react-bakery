a9d177494231c9d1376a083775a08de9526fad17
Slava <kuncovs19@gmail.com>
2023-10-18 03:18:37 +0300

    add positioning

--- src/components/Buttons/Button.jsx
+++ src/components/Buttons/Button.jsx
@@ -2,7 +2,7 @@ import classNames from "classnames";
 import { Link } from "react-router-dom";
 
 
-export default function Button ({ isDark, to, children }) {
+export default function Button ({ isDark = 'true', to = '/', position = 'left', children }) {
 	
 	return (
 		<Link
@@ -12,6 +12,9 @@ export default function Button ({ isDark, to, children }) {
 					'px-6 lg:px-8 py-[10px] rounded-md text-lg lg:text-xl hover:shadow-xl transition-all shadow-none duration-300',
 					{'bg-brown hover:bg-[#3c1719] text-[#FBEDCD] active:bg-[#4a1d1fd6]' : isDark},
 					{'bg-transparent border border-[#4A1D1F]' : !isDark},
+					{'ml-auto' : position === 'right'},
+					{'mr-auto' : position === 'left'},
+					{'mx-auto' : position === 'center'},
 				)
 			}
 		>
